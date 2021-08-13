df36839bf963bb7f93c81a5dc0cc0364ae5c9add
ma1gus <now-kirill@ya.ru>
2021-08-13 19:35:38 +0300

    Android-game-Bouncing-Ball
    Mobile game

---
+++ Bouncing Ball/Assets/Scripts/Ball/Ball.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out PlatformSegment platformSegment))
+        {
+            other.GetComponentInParent<Platform>().Break();
+        }
+    }
+}
